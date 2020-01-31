2f7603033b7af9af76e9cf038a157964177b49ee
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-31 09:09:15 -0800

    login is working and storing token to localStorage

--- client/src/App.js
+++ client/src/App.js
@@ -3,8 +3,11 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from './components/BubblePage';
 
 function App() {
+
+
   return (
     <Router>
       <div className="App">
@@ -13,6 +16,8 @@ function App() {
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
+        <Route path='/bubble-page' component={BubblePage} />
+        
       </div>
     </Router>
   );
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,58 @@
-import React from "react";
+import React, { Component, useState } from "react";
+import axios from 'axios';
 
-const Login = () => {
+
+// import { axiosWithAuth } from '../utils/axiosWithAuth';
+
+class Login extends React.Component  {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  }
+
+
+    submitLogin = (e) => {
+      e.preventDefault();
+      axios
+      .post('http://localhost:5000/api/login', this.state.credentials)
+      .then(res => {
+        localStorage.setItem('token', res.data.payload)
+        console.log(res)
+        this.props.history.push('/bubble-page');
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    }
+
+  onChangeHandler = e => {
+    e.preventDefault();
+    this.setState({
+      credentials: {
+      ...this.state.credentials,
+      [e.target.name]: e.target.value
+      }
+    })
+  }
+
+render() {
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
+      <form onSubmit={this.submitLogin}>
+        <input value={this.state.credentials.username} type='text' placeholder='username' name='username' onChange={this.onChangeHandler} />
+        <input value={this.state.credentials.password} type='password' placeholder='password' name='password' onChange={this.onChangeHandler} />
+        <button type='submit'>login</button>
+      </form>
+
     </>
   );
 };
+}
 
 export default Login;
